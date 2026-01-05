5ed02e88d964303057ae600a374f686d0844d877
Samrat-Debugger <md721023@gmail.com>
2026-01-05 17:38:24 +0600

    first challenge done

---
+++ challenge-1/app.js
@@ -0,0 +1,6 @@
+const paragraph = document.querySelector("p");
+const button = document.querySelector("button");
+
+button.addEventListener("click", function () {
+  paragraph.innerText = "Be an web developer";
+});
