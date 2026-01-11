e7b58c6c9943fa59b87af329a05bf3fd13bc3b70
Samrat-Debugger <md721023@gmail.com>
2026-01-12 00:44:08 +0600

    challenge 50 done

---
+++ challenge-50/app.js
@@ -0,0 +1,9 @@
+const nextColor = document.querySelector("button");
+let lists = document.querySelectorAll("li");
+
+nextColor.addEventListener("click", () => {
+  for (const list of lists) {
+    let randomColor = "#" + Math.round(Math.random() * 2500000).toString(16);
+    list.style.borderColor = randomColor;
+  }
+});
